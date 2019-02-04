ccb4513ac4a06738871ba6f2313e971ff7e9a2eb
Luke A <alvarengalucas55@gmail.com>
2019-02-04 12:31:27 -0200

    Add Typing Error Checking

--- static/js/ui.js
+++ static/js/ui.js
@@ -1,42 +1,16 @@
 var i = 0;
 var intId;
 
-function toggleButton()
-{
-    i = 0;
-    $('#prog-bar').css('width', '3%');
-
-    button = document.getElementById('type-button');
-    buttonClasses = [].slice.apply(button.classList);
-    button.classList.toggle('btn-success');
-    button.classList.toggle('btn-danger');
-
-    if (buttonClasses.includes('btn-success'))
-    {
-        $('#test-active').css('display', 'block');
-        $('#type-button').text('Stop');
-        intId = setInterval(prog, 100, 0.1);
-    }
-    else
-    {
-        $('#test-active').css('display', 'none');
-        $('#type-button').text('Start');
-        clearInterval(intId);
-    }
-}
-
-function prog(int)
-{
-    if (i >= 100)
-    {
+function progress(int) {
+    if (i >= 100) {
         $('#prog-bar').text('Done!');
         $('#prog-bar').css('background-color', '#28a745');
+        $('#test-active').css('display', 'none');
         $('#finished-alert').show();
         clearInterval(intId);
         return;
     }
-    else if (i > 3)
-    {
+    else if (i > 3) {
         $('#prog-bar').css('width', (i + '%'));
     }
     $('#prog-bar').text(i.toFixed(0) + '%');
